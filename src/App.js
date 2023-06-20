cac4c507f70b10f6740232f05691f3750abb0b9b
Paulo <paulocorreapc93@gmail.com>
2023-06-20 12:46:32 -0400

    importada biblioteca phosphor-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom ";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" />
           <Route path="/cart" />
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to={"/"}>Shop</Link>
+        <Link to={"/cart"}></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
