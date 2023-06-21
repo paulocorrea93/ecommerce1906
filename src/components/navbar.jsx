0b48bc33b8f7e0646749afce24d6361f40a5d70d
Paulo <paulocorreapc93@gmail.com>
2023-06-21 11:13:42 -0400

    importada biblioteca material ui e adicionado icone do carrinho

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,15 @@
+import { ShoppingCart } from "@mui/icons-material";
 import React from "react";
 import { Link } from "react-router-dom";
-import { ShoppingCart } from "phosphor-react";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
         <Link to={"/"}>Shop</Link>
-        <Link to={"/cart"}></Link>
+        <Link to={"/cart"}>
+          <ShoppingCart/>
+        </Link>
       </div>
     </div>
   );
