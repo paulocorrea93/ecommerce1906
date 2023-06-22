3558e119de25c278b5c1f6dc4c9e6ec626e15fbf
Paulo <paulocorreapc93@gmail.com>
2023-06-22 14:59:17 -0400

    criada função para somar os itens no carrinho

--- src/context/ShopContextProvider.jsx
+++ src/context/ShopContextProvider.jsx
@@ -4,16 +4,19 @@ import { PRODUCTS } from "../products";
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {}
-    for (let i = 1; i < PRODUCTS.length; i++){
-        cart[i] = 0;
-    }
-    return cart;
-}
-
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] +1}))
+  };
+
   return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
 
