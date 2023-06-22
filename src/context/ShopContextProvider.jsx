484c627beed6f292af0c4ec52e299122944002d8
Paulo <paulocorreapc93@gmail.com>
2023-06-22 14:55:35 -0400

    criada função para rastrear os itens do shopping para o carrinho

--- src/context/ShopContextProvider.jsx
+++ src/context/ShopContextProvider.jsx
@@ -1,9 +1,20 @@
 import React, { createContext } from "react";
+import { PRODUCTS } from "../products";
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = () => {
-  return <div>Shop-Context</div>;
+const getDefaultCart = () => {
+    let cart = {}
+    for (let i = 1; i < PRODUCTS.length; i++){
+        cart[i] = 0;
+    }
+    return cart;
+}
+
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState();
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
 
 export default ShopContextProvider;
